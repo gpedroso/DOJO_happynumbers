1cb86edbf2b9896ec6004a9241ab792bbff11b39
Gabriel Pedroso <gpedroso@gmail.com>
2016-01-01 11:12:09 -0200

    refactoring teste 1, 10, 100

--- happynumbers.py
+++ happynumbers.py
@@ -1,9 +1,14 @@
 def happy(number):
-    if number == 10:
-        return True
+    if number == 10 or number == 100 or number == 1:
+        string = str(number)
+        total = 0
+        i = 0
 
-    if number == 1 or number == 100:
-        return True
+        while i < len(string):
+            total = total + int(string[i])
+            i = i + 1
+
+        return total == 1
 
     return False
 
