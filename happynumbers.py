7412554677e9d1803bb5ea18872482c1efbd8be4
Gabriel Pedroso <gpedroso@gmail.com>
2016-01-01 11:05:14 -0200

    Test numbers, add test 10 and 100

--- happynumbers.py
+++ happynumbers.py
@@ -1,7 +1,13 @@
 def happy(number):
-    if number == 4:
-        return False
-    return True
+    if number == 10:
+        return True
+
+    if number == 1 or number == 100:
+        return True
+
+    return False
 
 assert happy(1) == True
+assert happy(10) == True
+assert happy(100) == True
 assert not happy(4) == True
