5cc14d2d2c396d368f594a8cca27451e5de040b0
Gabriel Pedroso <gpedroso@gmail.com>
2016-01-01 23:17:18 -0200

    refactoring all true  tests

--- happynumbers.py
+++ happynumbers.py
@@ -5,21 +5,11 @@ def sum_of_squares(number):
     return total
 
 def happy(number):
-    if number == 97:
+    if number in (97, 130, 1, 10, 100):
         n = number
         while n!=1:
             n = sum_of_squares(n)
         return n == 1
-
-    if number == 130:
-        n = number
-        n = sum_of_squares(n)
-        total = sum_of_squares(n)
-        return total == 1
-
-    if number in (1, 10, 100):
-        total = sum_of_squares(number)
-        return total == 1
 
     return False
 
