70f6c6eda7f16e09e5b81896cbd53f9053c3e29b
Gabriel Pedroso <gpedroso@gmail.com>
2016-01-01 23:32:23 -0200

    refactoring recursivity

--- happynumbers.py
+++ happynumbers.py
@@ -1,11 +1,8 @@
-def sum_of_squares(number):
-    return sum([int(char)**2 for char in str(number)])
-
 def happy(number):
     if number < 10:
         return number in (1, 7)
 
-    return (happy(sum_of_squares(number)))
+    return (happy(sum([int(char)**2 for char in str(number)])))
 
 assert all(happy(n) for n in (1,10,100,130,97))
 assert not all (happy(n) for n in (2,3,4,5,6,8,9))
